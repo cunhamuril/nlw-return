71a1a073bb4424445626b91aa68c46945b1de291
Murilo Cunha <murilo.sant@hotmail.com>
2022-05-09 11:28:21 -0300

    feat(server): nodemailer config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from "express";
+import nodemailer from "nodemailer";
+
 import { prisma } from "./prisma";
 
 interface FeedbackBody {
@@ -11,6 +13,15 @@ const app = express();
 
 app.use(express.json());
 
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "ac3a87712f324d",
+    pass: "f166510b103f3e",
+  },
+});
+
 app.post(
   "/feedbacks",
   async (req: Request<{}, {}, FeedbackBody>, res: Response) => {
@@ -22,6 +33,18 @@ app.post(
         comment,
         screenshot,
       },
+    });
+
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Murilo Cunha <murilo.sant@hotmail.com>",
+      subject: "Novo feedback",
+      html: [
+        '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
+        "</div>",
+      ].join("\n"),
     });
 
     return res.status(201).json({ data: feedback });
