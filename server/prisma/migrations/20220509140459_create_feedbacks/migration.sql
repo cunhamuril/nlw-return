bbcafda5478a20cd776280b409e22eed8c3ec3cb
Murilo Cunha <murilo.sant@hotmail.com>
2022-05-09 11:12:59 -0300

    feat(server): create feedbacks endpoint

--- server/prisma/migrations/20220509140459_create_feedbacks/migration.sql
+++ server/prisma/migrations/20220509140459_create_feedbacks/migration.sql
@@ -2,6 +2,6 @@
 CREATE TABLE "feedbacks" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "type" TEXT NOT NULL,
-    "commnet" TEXT NOT NULL,
+    "comment" TEXT NOT NULL,
     "screenshot" TEXT
 );
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,32 @@
 import express, { Request, Response } from "express";
+import { prisma } from "./prisma";
+
+interface FeedbackBody {
+  type: "BUG" | "IDEA" | "OTHER";
+  comment: string;
+  screenshot?: string;
+}
 
 const app = express();
+
+app.use(express.json());
+
+app.post(
+  "/feedbacks",
+  async (req: Request<{}, {}, FeedbackBody>, res: Response) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+      data: {
+        type,
+        comment,
+        screenshot,
+      },
+    });
+
+    return res.status(201).json({ data: feedback });
+  }
+);
 
 app.listen(3333, () => {
   console.log("HTTP server running!");
