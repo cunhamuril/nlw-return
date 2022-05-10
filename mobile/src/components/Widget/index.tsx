7f61391c7775310d1a9ff189fe160aedcd3e3dc8
Murilo Cunha <murilo.sant@hotmail.com>
2022-05-10 19:59:29 -0300

    feat(mobile): done options layout

---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,45 @@
+import React, { ReactElement, useRef } from "react";
+import { TouchableOpacity } from "react-native";
+import { ChatTeardropDots } from "phosphor-react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
+
+import { theme } from "../../theme";
+import { feedbackTypes } from "../../utils/feedbackTypes";
+
+import { Options } from "../Options";
+
+import { styles } from "./styles";
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand();
+  }
+
+  return (
+    <>
+      <TouchableOpacity style={styles.button} onPress={handleOpen}>
+        <ChatTeardropDots
+          size={24}
+          color={theme.colors.text_on_brand_color}
+          weight="bold"
+        />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        <Options />
+      </BottomSheet>
+    </>
+  );
+}
+
+export default gestureHandlerRootHOC(Widget);
