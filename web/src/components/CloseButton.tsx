cb4030492b0c4a9098f2ed345013fa0cd004f4c9
Murilo Cunha <murilo.sant@hotmail.com>
2022-05-11 11:52:40 -0300

    feat(web): aPI integration

--- web/src/components/CloseButton.tsx
+++ web/src/components/CloseButton.tsx
@@ -7,7 +7,7 @@ export function CloseButton() {
       className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100"
       title="Fechar formulário de feedback"
     >
-      <X className="w-4 h-4" weight="bold" />
+      <X className="w-4 h-4" weight="bold" />      
     </Popover.Button>
   );
 }
